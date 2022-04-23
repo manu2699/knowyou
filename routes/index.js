5e30eee04789cc9ff0da564ee65b4f705eb0a699
manu2699 <maneeshvijaykar@gmail.com>
2022-04-23 21:48:42 +0530

    🐛 lint issues

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const AuthRoutes = require("./auth");
 const UserRoute = require("./user");
-const KnowRoutes = require("./know");s
+const KnowRoutes = require("./know");
 
 router.use("/auth", AuthRoutes);
 router.use("/user", UserRoute);
